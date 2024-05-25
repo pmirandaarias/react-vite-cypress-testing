f403203cfa4e11d729391e22a042df8f90524597
Paulo Miranda <pmirandaarias@gmail.com>
2024-05-24 22:33:45 -0400

    Added more logos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,24 @@
 // src/pages/Home.jsx
 
 import React from 'react';
-import projectImage from '../../assets/react.svg';
+import reactImage from '../../assets/react.svg';
+import viteImage from '../../assets/vite.svg';
+import vitestImage from '../../assets/vitest.svg';
+import cypressImage from '../../assets/cypress.svg';
+import pnpmImage from '../../assets/pnpm.svg';
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Bienvenidos a nuestra aplicación</h1>
       <p className="subtitle">Explora nuestras funcionalidades increíbles</p>
-      <img src={projectImage} alt="Proyecto" className="project-image" />
+      <div className="image-container">
+        <img src={reactImage} alt="Proyecto" className="project-image"/>
+        <img src={viteImage} alt="Segundo Proyecto" className="project-image"/>
+        <img src={vitestImage} alt="Segundo Proyecto" className="project-image"/>
+        <img src={cypressImage} alt="Segundo Proyecto" className="project-image"/>
+        <img src={pnpmImage} alt="Segundo Proyecto" className="project-image"/>
+      </div>
       <div className="content">
         <p>Este proyecto utiliza las siguientes tecnologías:</p>
         <ul>
